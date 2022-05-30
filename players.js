ac052c57d917f8cb5a8bee15378a974967cf80c8
Your Name <you@example.com>
2022-05-30 11:43:21 +0300

    winner_fix

--- players.js
+++ players.js
@@ -32,6 +32,14 @@ function showPlayers(totalPlayers) {
     }
 }
 
+let playerScores = {
+    player1_score: 0,
+    player2_score: 0,
+    player3_score: 0,
+    player4_score: 0,
+    player5_score: 0,
+    player6_score: 0
+}
 // обработка очков
 function countCrussaders(form) {
     let playerScore = document.querySelectorAll('.player_score')
@@ -39,13 +47,38 @@ function countCrussaders(form) {
         if(span.id == form.name) {
             totalScore.innerHTML = totalScore.innerHTML - +form.crusaders.value
             let playerNewScore = +span.innerHTML + +form.crusaders.value;
+            playerScores[span.id] = playerNewScore;
             span.innerHTML = playerNewScore;
-            // выйгравший
-            if(totalScore.innerHTML <= 0) {
-                span.parentElement.classList.add('winner');
-                span.parentElement.insertAdjacentHTML("beforeend", `<img src='https://cdn.icon-icons.com/icons2/2699/PNG/512/atlassian_jira_logo_icon_170511.png'>`)
-                totalScore.innerHTML = score; 
-            }
+            form.crusaders.value = 0;
         }
     });
+}
+
+//раздача реликвий
+function getWinner() {
+    if(totalScore.innerHTML <= 0) {       
+        let winner;
+
+        
+        let max = Object.entries(playerScores).sort(function(a,b){ 
+            return +b[b.length-1] - a[a.length-1]
+        })
+       // let max = Object.entries(playerScores).reduce((acc, curr) => acc[1] >= curr[1] ? acc : curr)[0];
+        console.log(max.slice(0, 2))
+        if(max[0] > max[1]) {
+            winner = max[0]
+        } else {
+            winner = max
+        }
+         
+
+        document.getElementById(winner).parentElement.insertAdjacentHTML("beforeend", `<img src='https://cdn.icon-icons.com/icons2/2699/PNG/512/atlassian_jira_logo_icon_170511.png'>`)
+        totalScore.innerHTML = score; 
+    } else {
+        let notification = document.createElement('div');
+        notification.className = "notification";
+        notification.innerHTML = 'Не дам! Святая земля еще не покорена!';
+        document.getElementById('winnerBtn').parentElement.append(notification);
+        setTimeout(() => notification.remove(), 2500);
+    }
 }
